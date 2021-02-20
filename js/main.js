c4e9a634c59113bb09aa24422f85f3b89331ec7a
Berk selvi <berkslv@gmail.com>
2021-02-20 15:43:45 +0300

    Test purpose - notification test deleted

--- js/main.js
+++ js/main.js
@@ -27,28 +27,6 @@ const paddingHorizontal = document.getElementsByClassName("px-5");
     }
 })();
 
-function randomNotification() {
-    var notifTitle = 'bu bir test mesajıdır';
-    var notifBody = 'Test mesajıdır';
-    var notifImg = 'images/favicon.png';
-    var options = {
-        body: notifBody,
-        icon: notifImg
-    }
-    var notif = new Notification(notifTitle, options);
-}
-
-document.getElementById("notif").addEventListener('click', function(e) {
-    Notification.requestPermission().then(function(result) {
-        if(result === 'granted') {
-            setTimeout(() => {
-                randomNotification();
-            }, 4000);
-        }
-    });
-});
-
-
 /**
  * Makes class changes to handle clicking the tabs.
  * @param {this} e For the clicked element itself 
