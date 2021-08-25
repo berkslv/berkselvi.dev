318d696b8ae781cbd018006871ee3c02effccb7f
berkslv <berkslv@gmail.com>
2021-08-26 00:50:35 +0300

    Finished main layout, will be added some js functionalities.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,10 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.js"],
   presets: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     screens: {
+      xs: "475px",
       sm: "640px",
       md: "768px",
       lg: "1024px",
